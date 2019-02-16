8212a4d9b19bd5f737e50919ea4095e7f8089be8
nandoplayart <nandoplayart@hotmail.com>
2019-02-16 11:10:36 -0200

    implementação service bus

--- GeekBurger.LabelLoader/GeekBurger.LabelLoader.ExtractionOfIngredients/Domain/Services/SendIngredientsService.cs
+++ GeekBurger.LabelLoader/GeekBurger.LabelLoader.ExtractionOfIngredients/Domain/Services/SendIngredientsService.cs
@@ -1,9 +1,13 @@
 ﻿using GeekBurger.LabelLoader.Contract;
 using GeekBurger.LabelLoader.ExtractionOfIngredients.Domain.Interfaces;
 using GeekBurger.LabelLoader.ExtractionOfIngredients.Domain.Models;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace GeekBurger.LabelLoader.ExtractionOfIngredients.Domain.Services
@@ -14,12 +18,34 @@ namespace GeekBurger.LabelLoader.ExtractionOfIngredients.Domain.Services
         {
             try
             {
-
+                string messageBody = JsonConvert.SerializeObject(labelImageAdded);
+                SendMessagesAsync(messageBody).Wait();
             }
             catch (Exception)
             {
                 throw new Exception("Falha ao enviar ingredientes!");
-            }            
+            }
+        }
+
+        private async Task SendMessagesAsync(string messageBody)
+        {
+
+            var _config = GetConfigurationRoot();
+            string serviceBusConnectionString = _config.GetConnectionString("serviceBus:connectionString");
+            string qeueName = _config.GetConnectionString("serviceBus:subscriptionId");
+            var queueClient = new QueueClient(serviceBusConnectionString, qeueName);
+            var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+            await queueClient.SendAsync(message);
+
+        }
+
+        private IConfigurationRoot GetConfigurationRoot()
+        {
+            var builder = new ConfigurationBuilder()
+               .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+               .AddEnvironmentVariables();
+            return builder.Build();
+
         }
     }
 }
