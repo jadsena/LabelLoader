a73073038fb846474511aa3bd2b062ab413980f3
logonrmlocal <logonrmlocal@L2201MICRO35>
2019-02-16 14:46:55 -0200

    Chamada Api  com console Finalizada

--- GeekBurger.LabelLoader/GeekBurger.LabelLoader/Program.cs
+++ GeekBurger.LabelLoader/GeekBurger.LabelLoader/Program.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using GeekBurger.LabelLoader.Contract;
+using Microsoft.Extensions.Configuration;
 
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net.Http;
 using System.Security.Permissions;
+using System.Threading.Tasks;
 
 namespace GeekBurger.LabelLoader
 {
@@ -13,6 +16,7 @@ namespace GeekBurger.LabelLoader
         private List<string> Extensoes { get; }
         private FileSystemWatcher Watcher { get; set; }
         private IConfiguration Config { get; }
+        private string _urlBase { get; }
         static void Main(string[] args)
         {
             Console.WriteLine("LabelLoader 1.0.0");
@@ -29,8 +33,9 @@ namespace GeekBurger.LabelLoader
                 .Build();
             DirectoryName = Config.GetSection("LabelImagens:Diretorio").Value;
             Extensoes = Config.GetSection("LabelImagens:Extensoes").Get<List<string>>();
+            _urlBase = Config.GetSection("LabelImagens:UrlBase").Value;
             Console.WriteLine($"Diretório para imagens: {DirectoryName}");
-            Console.WriteLine($"Extensoes das imagens:  {string.Join(",",Extensoes)}");
+            Console.WriteLine($"Extensoes das imagens:  {string.Join(",", Extensoes)}");
             if (!Directory.Exists(DirectoryName)) Directory.CreateDirectory(DirectoryName);
         }
 
@@ -62,10 +67,55 @@ namespace GeekBurger.LabelLoader
             }
         }
 
-        private void OnChanged(object sender, FileSystemEventArgs e)
+        private async void OnChanged(object sender, FileSystemEventArgs e)
         {
-            if (!Extensoes.Contains(Path.GetExtension(e.Name).ToLower().Replace(".",""))) return;
+            if (!Extensoes.Contains(Path.GetExtension(e.Name).ToLower().Replace(".", ""))) return;
+
+
+            await EnviarParaApi(new FileInfo(e.FullPath));
+
             Console.WriteLine($"Arquivo: {e.Name}, FullPath: {e.FullPath}, ChangeType: {e.ChangeType}");
         }
+
+        private Uri Url(string url)
+        {
+            var endpoint = new Uri(new Uri(_urlBase), url);
+            return endpoint;
+        }
+
+        private async Task<bool> EnviarParaApi(FileInfo arquivo)
+        {
+            try
+            {
+                var arquivoConvertido = File.ReadAllBytes(arquivo.FullName);
+                AddLabelImage imagem = new AddLabelImage
+                {
+                    ItemName = arquivo.Name,
+                    File = Convert.ToBase64String(arquivoConvertido)
+                };
+
+               
+                using (var client = new HttpClient())
+                {
+                    var resposta = await client.PostAsJsonAsync(Url("api/Ingredient").ToString(), imagem);
+                    if (resposta.IsSuccessStatusCode)
+                    {
+
+                        return true;
+
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
     }
 }
